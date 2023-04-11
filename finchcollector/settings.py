7993eb3a549b02ea6053cba171e44f22d64f4cba
nohatekate <kateiscoding@gmail.com>
2023-04-11 18:10:21 -0500

    define URL, View Function and Template for Home

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
 
 SECRET_KEY = config("SECRET_KEY")
-
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+finches = [
+  {'name': 'Dan', 'breed': 'finch', 'description': 'loud', 'age': 3},
+  {'name': 'Kate', 'breed': 'finch', 'description': 'quiet', 'age': 2},
+]
+
+def home(request):
+    return render(request, 'home.html')
