72539729978d35cc4983bb3c9d15b94d35f7b561
nohatekate <kateiscoding@gmail.com>
2023-04-11 18:51:47 -0500

    Define URL, View Function and Templates for about and quarters_index, Template Inheritence(Partials)

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-finches = [
-  {'name': 'Dan', 'breed': 'finch', 'description': 'loud', 'age': 3},
-  {'name': 'Kate', 'breed': 'finch', 'description': 'quiet', 'age': 2},
+quarters = [
+  {'state': 'Delaware', 'release_date': 'January 4, 1999', 'description': 'Caesar Rodney on horseback Captions: \"The First State\", \"Caesar Rodney\"', 'statehood_date': 'December 7, 1787'},
+  {'state': 'Pennsylvania', 'release_date': 'March 8, 1999', 'description': 'Commonwealth statue, state outline, keystone Caption: \"Virtue, Liberty, Independence\"', 'statehood_date': 'December 12, 1787'},
 ]
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def quarters_index(request):
+    return render(request, 'quarters/index.html', {
+        'quarters': quarters
+    })
