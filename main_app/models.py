478c6d27ea29b685efe8103c716f4eecbbeffe78
nohatekate <kateiscoding@gmail.com>
2023-04-12 15:21:38 -0500

    created Model and one Quarter after much time debugging

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Quarter(models.Model):
+    state_name = models.CharField(max_length=50)
+    release_date = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    statehood_date = models.CharField(max_length=50)
+    engraver = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.state_name
